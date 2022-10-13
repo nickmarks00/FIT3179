0a6480fa4f74b322fe68bb3e8b6c5e06f5b73471
Nick Marks <nickmarks00@gmail.com>
2022-10-13 14:15:33 +1100

    Fixes

--- vegaLiteVis.js
+++ vegaLiteVis.js
@@ -1,9 +1,9 @@
-const vg1 = "src/energy-cons-country.vg.json";
+const vg1 = "energy-cons-country.vg.json";
 vegaEmbed("#cons-country", vg1)
   .then((result) => {})
   .catch(console.error);
 
-const vg2 = "src/energy-cons-map.vg.json";
+const vg2 = "energy-cons-map.vg.json";
 vegaEmbed("#cons-map", vg2)
   .then((result) => {})
   .catch(console.error);

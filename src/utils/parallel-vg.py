beafb2e1c16ef414563a80d7e18d35636a448a53
Nick Marks <nickmarks00@gmail.com>
2022-10-16 17:02:49 +1100

    Fix parallel.csv

--- src/utils/parallel-vg.py
+++ src/utils/parallel-vg.py
@@ -16,7 +16,7 @@ df = pd.merge(df,country_cont_df, how='left', on=['Entity'])
 keys = ['GDP per capita', 'Fossil fuel electricity per capita (kWh)', 'Nuclear electricity per capita (kWh)', 'Renewable electricity per capita (kWh)']
 for key in keys:
     df = df[~df[key].isnull()]
-    # df = df[df[key] != 0]
+    df = df[df[key] != 0]
 
 df['key'] = 'GDP per capita'
 res = pd.DataFrame()
@@ -25,22 +25,12 @@ for key in keys:
     tmp = copy.deepcopy(df)
     tmp['key'] = key
     tmp['value'] = tmp[key]
+    min_val, max_val = tmp[key].min(), tmp[key].max()
+    tmp['min'], tmp['max'] = min_val, max_val
+    tmp['mid'] = (max_val + min_val) / 2
+    tmp['norm_val'] = (tmp['value'] - tmp['min']) / (max_val - min_val)
     res = pd.concat([res, tmp], axis=0)
 
-# Duplicate each row four times
-# df['value'] = df['GDP per capita']
-# df1 = copy.deepcopy(df)
-# df2 = copy.deepcopy(df)
-# df3 = copy.deepcopy(df)
 
-
-# df1['key'] = 'Fossil fuel electricity per capita (kWh)'
-# df1['value'] = df1['Fossil fuel electricity per capita (kWh)']
-# df2['key'] = 'Nuclear electricity per capita (kWh)'
-# df2['value'] = df2['Nuclear electricity per capita (kWh)']
-# df3['key'] = 'Renewable electricity per capita (kWh)'
-# df3['value'] = df3['Renewable electricity per capita (kWh)']
-
-# df = pd.concat([df,df1,df2,df3], axis=0)
 
 res.to_csv(os.path.dirname(__file__) + '/../data/parallel.csv')
